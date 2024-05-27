dceb4745ec55f47dd6b257778fa34f1ccc7a77a4
Amir <amiram19@yandex.ru>
2024-05-27 16:56:33 +0300

    Lesson 3

--- products/views.py
+++ products/views.py
@@ -2,5 +2,38 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+# функции = контроллеры = вьюхи
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the store's index view.")
+    context = {
+        'title': 'Store',
+    }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    context = {
+        'title': 'Store - Каталог',
+        'products': [
+            {
+                'image': '/static/vendor/img/products/Adidas-hoodie.png',
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 6090,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+            },
+            {
+                'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
+                'name': 'Синяя куртка The North Face',
+                'price': 23725,
+                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
+            },
+            {
+                'image': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
+                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
+                'price': 3390,
+                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
+            }
+        ]
+    }
+    return render(request, 'products/products.html')

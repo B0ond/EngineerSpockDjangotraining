d35fe756df53860078b94ab327677fa730b42b05
Amir <amiram19@yandex.ru>
2024-06-13 18:59:05 +0300

    Lesson 4 1

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# model == таблица бд
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(Null=True, blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(Null=True, blank=True)

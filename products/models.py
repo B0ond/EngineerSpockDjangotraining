7227a6d7a2d7a3de0f3a7173f2a9ce4cafede3e7
B0ond <amiram19@yandex.ru>
2024-06-13 23:14:49 +0300

    4 2

--- products/models.py
+++ products/models.py
@@ -12,3 +12,7 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField(Null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products/')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)

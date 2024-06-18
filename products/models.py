438e836ce72f41be3e37ce39614f7a458da79192
Amir <amiram19@yandex.ru>
2024-06-18 18:02:02 +0300

    Lesson 5 2

--- products/models.py
+++ products/models.py
@@ -8,6 +8,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)

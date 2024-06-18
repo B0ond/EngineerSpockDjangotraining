f993935f6b2bf519ae95f405e4605a5dfe918c9b
Amir <amiram19@yandex.ru>
2024-06-18 13:07:43 +0300

    Lesson 5 1

--- products/models.py
+++ products/models.py
@@ -6,12 +6,12 @@ from django.db import models
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    description = models.TextField(Null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
-    description = models.TextField(Null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products/')

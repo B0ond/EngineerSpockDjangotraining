7c7b8ccf57d6b421516c2b195ac0937f56e4e88b
Amir <amiram19@yandex.ru>
2024-06-24 17:00:07 +0300

    Lesson 5 5

--- products/models.py
+++ products/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='products_images/')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
     def __str__(self):
